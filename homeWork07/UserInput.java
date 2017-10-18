c6558234c1e6a9744b1c28369484ab39a105f869
Maksym <31632075+Tachyonn@users.noreply.github.com>
2017-10-18 20:04:51 +0300

    Add files via upload

--- homeWork07/UserInput.java
+++ homeWork07/UserInput.java
@@ -14,12 +14,21 @@ public class UserInput {
         try {
             System.out.println("Enter first name:");
             String firstName = reader.readLine();
+            if (firstName == "") {
+                firstName = "default name";
+            }
 
             System.out.println("Enter last name:");
             String lastName = reader.readLine();
+            if (lastName == "") {
+                lastName = "default lastname";
+            }
 
             System.out.println("Enter sex - male or female:");
             String sexInput = reader.readLine();
+            if (sexInput == "") {
+                sexInput = "male";
+            }
             Sex sex;
             if (sexInput.equalsIgnoreCase("male")) {
                 sex = Sex.MALE;
@@ -31,12 +40,21 @@ public class UserInput {
 
             System.out.println("Enter age:");
             int age = Integer.parseInt(reader.readLine());
+            if (age < 16) {
+                age = 16;
+            }
 
             System.out.println("Enter course:");
             String course = reader.readLine();
+            if (course == "") {
+                course = "default course";
+            }
 
             System.out.println("Enter thesis:");
             String thesis = reader.readLine();
+            if (thesis == "") {
+                thesis = "default thesis";
+            }
 
             return new Student(firstName, lastName, sex, age, course, thesis);
 
